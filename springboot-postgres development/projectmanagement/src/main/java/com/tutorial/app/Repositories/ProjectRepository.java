9ee5dbe1ac9607714c55a3ef17053e237c97130e
Sithija Shehara <nsithijashehara@gmail.com>
2026-02-19 23:44:23 +0530

    initial commit

--- springboot-postgres development/projectmanagement/src/main/java/com/tutorial/app/Repositories/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tutorial.app.Repositories;
-
-import com.tutorial.app.Models.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
--- springboot-postgres development/projectmanagement/src/main/java/com/tutorial/app/Repositories/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tutorial.app.Repositories;
-
-import com.tutorial.app.Models.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket,Long> {
-}
